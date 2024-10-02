f704eeaf99b912a451e9b3c0ba2a2880828c68d6
Shangyint <shangyin@berkeley.edu>
2024-10-02 12:52:22 -0700

    Update evaluate for humaneval

--- langProBe/humaneval/humaneval_eval.py
+++ langProBe/humaneval/humaneval_eval.py
@@ -10,5 +10,6 @@ evaluate_naive_program = EvaluateBench(
     human_eval_bench, NaiveCodeGenerator(), human_eval_evaluate
 )
 
-with dspy.context(lm=dspy.OpenAI(model="gpt-3.5-turbo", max_tokens=1000)):
-    evaluate_naive_program.evaluate()
+evaluate_naive_program.evaluate(
+    dspy_config=dict(lm=dspy.OpenAI(model="gpt-3.5-turbo", max_tokens=1000))
+)
