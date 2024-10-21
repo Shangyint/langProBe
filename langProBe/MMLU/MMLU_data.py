47d03851c5c19c12ad66a19cd94556d17b519973
Liheng Lai <liheng@berkeley.edu>
2024-10-21 14:24:14 -0700

    MMLU with Main integration

--- langProBe/MMLU/MMLU_data.py
+++ langProBe/MMLU/MMLU_data.py
@@ -6,25 +6,24 @@ from datasets import load_dataset
 class MMLUBench(Benchmark):
     def init_dataset(self):
         self.raw_datasets = load_dataset("cais/mmlu", "all")
-        
-    def create_splits(self):
+
         option_map = {
             0: "A", 
             1: "B",
             2: "C", 
             3: "D"
         }
-        self.test_set, self.dev_set, self.train_set = [], [], []
+        self.dataset = []
 
         for x in self.raw_datasets['test']:
             x['question'] = "\n".join([x['question'], "A. " + x['choices'][0], "B. " + x['choices'][1], "C. " + x['choices'][2], "D. " + x['choices'][3]])
             x['answer'] = option_map[x['answer']]
-            self.test_set.append(dspy.Example(**x).with_inputs("question"))
-        for x in self.raw_datasets['validation']:
-            x['question'] = "\n".join([x['question'], "A. " + x['choices'][0], "B. " + x['choices'][1], "C. " + x['choices'][2], "D. " + x['choices'][3]])
-            x['answer'] = option_map[x['answer']]
-            self.dev_set.append(dspy.Example(**x).with_inputs("question"))
-        for x in self.raw_datasets['dev']:
-            x['question'] = "\n".join([x['question'], "A. " + x['choices'][0], "B. " + x['choices'][1], "C. " + x['choices'][2], "D. " + x['choices'][3]])
-            x['answer'] = option_map[x['answer']]
-            self.train_set.append(dspy.Example(**x).with_inputs("question"))
+            self.dataset.append(dspy.Example(**x).with_inputs("question"))
+        # for x in self.raw_datasets['validation']:
+        #     x['question'] = "\n".join([x['question'], "A. " + x['choices'][0], "B. " + x['choices'][1], "C. " + x['choices'][2], "D. " + x['choices'][3]])
+        #     x['answer'] = option_map[x['answer']]
+        #     self.dataset.append(dspy.Example(**x).with_inputs("question"))
+        # for x in self.raw_datasets['dev']:
+        #     x['question'] = "\n".join([x['question'], "A. " + x['choices'][0], "B. " + x['choices'][1], "C. " + x['choices'][2], "D. " + x['choices'][3]])
+        #     x['answer'] = option_map[x['answer']]
+        #     self.dataset.append(dspy.Example(**x).with_inputs("question"))
--- langProBe/MMLU/__init__.py
+++ langProBe/MMLU/__init__.py
@@ -1,15 +1,17 @@
 from typing import Any, Callable
 from .MMLU_data import MMLUBench
 from .MMLU_program import CoT, RAG, SimplifiedBaleen
-from langProBe.benchmark import Benchmark
-import dspy
+from langProBe.benchmark import Benchmark, BenchmarkMeta
 
 benchmark: Callable[[], Benchmark] = MMLUBench
 programs = [CoT, RAG, SimplifiedBaleen]
-# programs = [ SimplifiedBaleen]
 
 def MMLU_metric(gt, pred, trace=None):
     pred_processed = pred.answer.split(".")[0]
     return gt.answer == pred_processed
 
-metric = MMLU_metric
+benchmark = [
+    BenchmarkMeta(
+        MMLUBench, programs, MMLU_metric
+    )
+]
