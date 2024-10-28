8870111547ff51e6ad348f653fcfcb79ac4be1a8
Shangyint <shangyin@berkeley.edu>
2024-10-28 12:25:44 -0700

    Change function to its embed class; prepend context field

--- langProBe/program.py
+++ langProBe/program.py
@@ -20,101 +20,97 @@ def default_input_to_query(**kwargs):
         )
 
 
-def RAGAdapter(
-    signature, retriever=dspy.Retrieve(k=3), input_to_query=default_input_to_query
-):
-    class RAG(dspy.Module):
-        def __init__(self):
-            self.retriver = retriever
-            self.prog = dspy.ChainOfThought(signature)
-            self.prog._predict.signature = self.prog._predict.signature.append(
-                "context", dspy.InputField(desc="may contain relevant facts")
-            )
-            self.prog._predict.extended_signature = self.prog._predict.signature
+class RAGAdapter(dspy.Module):
+    def __init__(
+        self,
+        signature,
+        retriever=dspy.Retrieve(k=3),
+        input_to_query=default_input_to_query,
+    ):
+        self.retriver = retriever
+        self.prog = dspy.ChainOfThought(signature)
+        self.input_to_query = input_to_query
 
-        def forward(self, **kwargs):
-            context = self.retriver(input_to_query(**kwargs)).passages
-            pred = self.prog(context=context, **kwargs)
-            return pred
+        self.prog._predict.signature = self.prog._predict.signature.prepend(
+            "context", dspy.InputField(desc="may contain relevant facts")
+        )
+        self.prog._predict.extended_signature = self.prog._predict.signature
 
-    return RAG()
+    def forward(self, **kwargs):
+        context = self.retriver(self.input_to_query(**kwargs)).passages
+        pred = self.prog(context=context, **kwargs)
+        return pred
 
 
-def SimplifiedBaleenAdapter(
-    signature, query_gen_input=None, retriever=dspy.Retrieve(k=2), max_hops=2
-):
-    """
-    args:
-        signature: The signature to the final generate module
-        query_gen_input: a list of keywords to be used as input to the query generation module
-        retriever: a retriever module to be used to retrieve relevant facts
-        max_hops: the number of hops to be used in the simplified
-        FIXME (shangyin) correctly handle query_gen_input
-    """
+class SimplifiedBaleenAdapter(dspy.Module):
+    def __init__(
+        self, signature, query_gen_input=None, retriever=dspy.Retrieve(k=2), max_hops=2
+    ):
+        """
+        args:
+            signature: The signature to the final generate module
+            query_gen_input: a list of keywords to be used as input to the query generation module
+            retriever: a retriever module to be used to retrieve relevant facts
+            max_hops: the number of hops to be used in the simplified
+            FIXME (shangyin) correctly handle query_gen_input
+        """
 
-    class SimplifiedBaleen(dspy.Module):
-        def __init__(self):
-            self.max_hops = max_hops
-            self.retriever = retriever
-            verified_signature = dspy.ensure_signature(signature)
-            verified_signature = verified_signature.append(
-                "context", dspy.InputField(desc="may contain relevant facts")
-            )
+        self.max_hops = max_hops
+        self.retriever = retriever
+        verified_signature = dspy.ensure_signature(signature)
+        verified_signature = verified_signature.prepend(
+            "context", dspy.InputField(desc="may contain relevant facts")
+        )
 
-            # remove the output field from the generate query signature
-            # generate_query should use a default instruction rather than instruction from the original signature
-            # FIXME (shangyin) fix the default signature.instructions
-            input_fields = verified_signature.input_fields
-            generate_query_signature = dspy.Signature(input_fields)
-            generate_query_signature = generate_query_signature.append(
-                "search_query", dspy.OutputField()
-            )
+        # remove the output field from the generate query signature
+        # generate_query should use a default instruction rather than instruction from the original signature
+        # FIXME (shangyin) fix the default signature.instructions
+        input_fields = verified_signature.input_fields
+        generate_query_signature = dspy.Signature(input_fields)
+        generate_query_signature = generate_query_signature.append(
+            "search_query", dspy.OutputField()
+        )
 
-            self.generate_query = [
-                dspy.ChainOfThought(generate_query_signature)
-                for _ in range(self.max_hops)
-            ]
-            self.generate_answer = dspy.ChainOfThought(verified_signature)
+        self.generate_query = [
+            dspy.ChainOfThought(generate_query_signature) for _ in range(self.max_hops)
+        ]
+        self.generate_answer = dspy.ChainOfThought(verified_signature)
 
-        def forward(self, **kwargs):
-            context = []
+    def forward(self, **kwargs):
+        context = []
 
-            for hop in range(self.max_hops):
-                query = self.generate_query[hop](context=context, **kwargs).search_query
-                passages = self.retriever(query).passages
-                context = deduplicate(context + passages)
+        for hop in range(self.max_hops):
+            query = self.generate_query[hop](context=context, **kwargs).search_query
+            passages = self.retriever(query).passages
+            context = deduplicate(context + passages)
 
-            pred = self.generate_answer(context=context, **kwargs)
-            return pred
-
-    return SimplifiedBaleen()
+        pred = self.generate_answer(context=context, **kwargs)
+        return pred
 
 
 #################################### Archon Adapters ####################################
 
 
-def ArchonGeneratorAdapter(signature, n=5):
+class ArchonGeneratorAdapter(dspy.Module):
     # https://github.com/ScalingIntelligence/Archon/blob/main/src/archon/completions/components/Generator.py
-    # For dspy, n responses are generated with a single model now.
-    # If desired, we can create a new module in dspy that uses multiple models to generate n responses.
-    class ArchonGenerator(dspy.Module):
-        def __init__(self):
-            verified_signature = dspy.ensure_signature(signature)
-            assert (
-                len(verified_signature.output_fields) == 1
-            ), "ArchonGenerator only supports a single output field"
 
-            self.prog = dspy.ChainOfThought(verified_signature, n=n)
-            self.output_field = list(verified_signature.output_fields.keys())[0]
+    def __init__(self, signature, n=5):
+        # For dspy, n responses are generated with a single model now.
+        # If desired, we can create a new module in dspy that uses multiple models to generate n responses.
+        verified_signature = dspy.ensure_signature(signature)
+        assert (
+            len(verified_signature.output_fields) == 1
+        ), "ArchonGenerator only supports a single output field"
 
-        def forward(self, **kwargs):
-            return self.prog(**kwargs)
+        self.prog = dspy.ChainOfThought(verified_signature, n=n)
+        self.output_field = list(verified_signature.output_fields.keys())[0]
 
-        def get_responses(self, **kwargs):
-            responses = self.prog(**kwargs).completions.__getattr__(self.output_field)
-            return responses
+    def forward(self, **kwargs):
+        return self.prog(**kwargs)
 
-    return ArchonGenerator()
+    def get_responses(self, **kwargs):
+        responses = self.prog(**kwargs).completions.__getattr__(self.output_field)
+        return responses
 
 
 def critic_responses_formatter(responses):
@@ -151,36 +147,33 @@ class CriticGenerator(dspy.Signature):
     )
 
 
-def ArchonCriticAdapter(signature, n=5):
+class ArchonCriticAdapter(dspy.Module):
     # https://github.com/ScalingIntelligence/Archon/blob/main/src/archon/completions/components/Critic.py
-    # signature should be the signature to the original generator module
 
-    class ArchonCritic(dspy.Module):
-        def __init__(self):
-            verified_signature = dspy.ensure_signature(signature)
-            assert (
-                len(verified_signature.output_fields) == 1
-            ), "ArchonCritic only supports a single output field"
-            self.signature = verified_signature
+    def __init__(self, signature, n=5):
+        # signature should be the signature to the original generator module
+        verified_signature = dspy.ensure_signature(signature)
+        assert (
+            len(verified_signature.output_fields) == 1
+        ), "ArchonCritic only supports a single output field"
+        self.signature = verified_signature
 
-            self.instructions = verified_signature.instructions
+        self.instructions = verified_signature.instructions
 
-            self.critic = dspy.Predict(CriticGenerator)
+        self.critic = dspy.Predict(CriticGenerator)
 
-        def forward(self, responses):
-            return self.critic(task_instructions=self.instructions, responses=responses)
+    def forward(self, responses):
+        return self.critic(task_instructions=self.instructions, responses=responses)
 
-        def get_critics(self, responses):
-            critics = self.critic(
-                task_instructions=self.instructions, responses=responses
-            ).critics
-            return critics
+    def get_critics(self, responses):
+        critics = self.critic(
+            task_instructions=self.instructions, responses=responses
+        ).critics
+        return critics
 
-        def parse_critics(self, critics):
-            segments = re.split(r"\[\d+\]", critics)
-            pass  # we may not need to parse the critics for now
-
-    return ArchonCritic()
+    def parse_critics(self, critics):
+        segments = re.split(r"\[\d+\]", critics)
+        pass  # we may not need to parse the critics for now
 
 
 class RankerGenerator(dspy.Signature):
@@ -203,80 +196,71 @@ class RankerGenerator(dspy.Signature):
     )
 
 
-def ArchonRankerAdapter(signature, n=5):
+class ArchonRankerAdapter(dspy.Module):
     # https://github.com/ScalingIntelligence/Archon/blob/main/src/archon/completions/components/prompts.py#L68
+    def __init__(self, signature, n=5):
+        verified_signature = dspy.ensure_signature(signature)
+        assert (
+            len(verified_signature.output_fields) == 1
+        ), "ArchonRanker only supports a single output field"
+        self.signature = verified_signature
+        self.instructions = verified_signature.instructions
 
-    class ArchonRanker(dspy.Module):
-        def __init__(self):
-            verified_signature = dspy.ensure_signature(signature)
-            assert (
-                len(verified_signature.output_fields) == 1
-            ), "ArchonRanker only supports a single output field"
-            self.signature = verified_signature
-            self.instructions = verified_signature.instructions
+        self.ranker = dspy.ChainOfThought(RankerGenerator)
 
-            self.ranker = dspy.ChainOfThought(RankerGenerator)
+    def forward(self, responses, **kwargs):
+        if kwargs.get("critics", None):
+            new_signature = self.ranker.extended_signature.append(
+                "critics",
+                dspy.InputField(
+                    desc="The critics (strength/weakness) for each response."
+                ),
+            )
+            new_signature.instructions += (
+                "and their provided critiques of strengths and weaknesses."
+            )
 
-        def forward(self, responses, **kwargs):
-            if kwargs.get("critics", None):
-                new_signature = self.ranker.extended_signature.append(
-                    "critics",
-                    dspy.InputField(
-                        desc="The critics (strength/weakness) for each response."
-                    ),
-                )
-                new_signature.instructions += (
-                    "and their provided critiques of strengths and weaknesses."
-                )
+            return self.ranker(
+                new_signature=new_signature,
+                task_instructions=self.instructions,
+                responses=responses,
+                **kwargs,
+            )
+        else:
+            return self.ranker(task_instructions=self.instructions, responses=responses)
 
-                return self.ranker(
-                    new_signature=new_signature,
-                    task_instructions=self.instructions,
-                    responses=responses,
-                    **kwargs,
-                )
-            else:
-                return self.ranker(
-                    task_instructions=self.instructions, responses=responses
-                )
-
-        def get_ranking(self, responses, **kwargs):
-            raw_ranking = self.forward(responses, **kwargs).ranking
-            ranks_str = re.findall(r"\[(\d+)\]", raw_ranking)
-            ranks = [int(r) for r in ranks_str]
-            if len(ranks) != len(responses):
-                dspy.logger.warning(
-                    "ArchonRanker: the number of responses and the number of ranks do not match."
-                )
-            return ranks
-
-    return ArchonRanker()
+    def get_ranking(self, responses, **kwargs):
+        raw_ranking = self.forward(responses, **kwargs).ranking
+        ranks_str = re.findall(r"\[(\d+)\]", raw_ranking)
+        ranks = [int(r) for r in ranks_str]
+        if len(ranks) != len(responses):
+            dspy.logger.warning(
+                "ArchonRanker: the number of responses and the number of ranks do not match."
+            )
+        return ranks
 
 
 # TODO(shangyin) new adapters from Archon to be added: Fuser, Verifier
 
 
-def ArchonPipelineExample1(signature, n=5):
-    class ArchonExample(dspy.Module):
-        def __init__(self):
-            verified_signature = dspy.ensure_signature(signature)
-            assert (
-                len(verified_signature.output_fields) == 1
-            ), "ArchonExample only supports a single output field"
-            self.signature = verified_signature
+class ArchonPipelineExample1(dspy.Module):
+    def __init__(self, signature, n=5):
+        verified_signature = dspy.ensure_signature(signature)
+        assert (
+            len(verified_signature.output_fields) == 1
+        ), "ArchonExample only supports a single output field"
+        self.signature = verified_signature
 
-            self.generator = ArchonGeneratorAdapter(self.signature, n)
-            self.critic = ArchonCriticAdapter(self.signature, n)
-            self.ranker = ArchonRankerAdapter(self.signature, n)
+        self.generator = ArchonGeneratorAdapter(self.signature, n)
+        self.critic = ArchonCriticAdapter(self.signature, n)
+        self.ranker = ArchonRankerAdapter(self.signature, n)
 
-        def forward(self, **kwargs):
-            responses = self.generator.get_responses(**kwargs)
-            print(responses)
-            critics = self.critic.get_critics(responses)
-            ranking = self.ranker.get_ranking(responses, critics=critics)
-            return responses[ranking[0]]
-
-    return ArchonExample()
+    def forward(self, **kwargs):
+        responses = self.generator.get_responses(**kwargs)
+        print(responses)
+        critics = self.critic.get_critics(responses)
+        ranking = self.ranker.get_ranking(responses, critics=critics)
+        return responses[ranking[0]]
 
 
 if __name__ == "__main__":
