c9cc287afed2fab991295779a49e3dab10ca80a6
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2024-10-28 22:34:20 -0700

    Fix dataset splits for AppWorld

--- langProBe/AppWorld/__init__.py
+++ langProBe/AppWorld/__init__.py
@@ -5,8 +5,8 @@ from .AppWorld_utils.appworld_metric import appworld_metric
 from .AppWorld_program import AppWorldReact
 
 from langProBe.benchmark import BenchmarkMeta
-import dspy
 
-benchmark = [BenchmarkMeta(AppWorldBench, [AppWorldReact], appworld_metric)]
+# Keep the benchmark size full, since the full test set size is 585.
+benchmark = [BenchmarkMeta(AppWorldBench, [AppWorldReact], appworld_metric, dataset_mode="full")]
 
 os.environ["APPWORLD_ROOT"] = os.path.join(os.getcwd(), "langProBe", "AppWorld")
