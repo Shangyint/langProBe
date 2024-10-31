daa3bbd1416b3b64a1f37a580e1cdbe63f29913a
Shangyint <shangyin@berkeley.edu>
2024-10-30 22:41:01 -0700

    fix irera data loading

--- langProBe/IReRa/irera_data.py
+++ langProBe/IReRa/irera_data.py
@@ -5,10 +5,6 @@ import subprocess
 
 class IReRaBench(Benchmark):
     def init_dataset(self):
-        subprocess.run(
-            ["bash", "langProBe/IReRa/load_data.sh"], capture_output=True, text=True
-        )
-
         (train_examples, validation_examples, test_examples, _, _, _) = load_data()
         self.dataset = train_examples + validation_examples
         self.test_set = test_examples
