47d87a992b7ba0027535659b85583586d514824f
Shangyint <shangyin@berkeley.edu>
2024-10-30 23:20:08 -0700

    make humaneval test and train disjoint

--- langProBe/humaneval/humaneval_data.py
+++ langProBe/humaneval/humaneval_data.py
@@ -13,9 +13,9 @@ class HumanEvalBench(Benchmark):
             for x in raw_datasets
         ]
 
-        self.test_set = [
-            dspy.Example(**x).with_inputs(
-                "prompt", "test", "entry_point", "canonical_solution", "task_id"
-            )
-            for x in raw_datasets
-        ]
+        self.test_set = self.dataset[len(self.dataset) // 2 :]
+        self.dataset = self.dataset[: len(self.dataset) // 2]
+
+        self.train_set = self.dataset[:15]
+        self.val_set = self.dataset[15:]
+        self.dev_set = self.dataset
