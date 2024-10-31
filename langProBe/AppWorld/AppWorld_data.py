9fd62f9031e0f6c2b11c272a48c6ff39c89e686d
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2024-10-31 11:15:48 -0700

    Fix random seed in AppWorld_data

--- langProBe/AppWorld/AppWorld_data.py
+++ langProBe/AppWorld/AppWorld_data.py
@@ -28,7 +28,7 @@ class AppWorldBench(Benchmark):
         num_scenarios_in_val = int((len(unique_scenario_ids_in_trainval)+1)/2)
 
         rng = random.Random()
-        rng.seed(1, version=2)
+        rng.seed(1)
         train_scenarios = rng.sample(unique_scenario_ids_in_trainval, len(unique_scenario_ids_in_trainval)-num_scenarios_in_val)
 
         self.train_set = [dspy.Example(task_id=task_id).with_inputs("task_id") for task_id in trainval_task_ids if task_id.split('_')[0] in train_scenarios]
