2333373fe53b9e543cc3eaf2fac3def404cac511
Shangyint <shangyin@berkeley.edu>
2024-11-13 00:05:57 -0800

    Added missing Iris programs

--- langProBe/Iris/Iris_program.py
+++ langProBe/Iris/Iris_program.py
@@ -11,7 +11,7 @@ class Sig(dspy.Signature):
     sepal_width = dspy.InputField()
     answer = dspy.OutputField(desc="setosa, versicolor, or virginica")
 
-
+IrisPredict = program.Predict(Sig)
 IrisCot = program.CoT(Sig)
 IrisGeneratorCriticRanker = program.GeneratorCriticRanker(Sig)
 IrisGeneratorCriticFuser = program.GeneratorCriticFuser(Sig)
