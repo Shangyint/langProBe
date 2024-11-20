d02bdf7bd3d1d1a622789c8da2821bf5d2af53ea
Lakshya A Agrawal <lakshyaaagrawal@berkeley.edu>
2024-11-19 16:32:09 -0800

    5/19 on AppWorld dev _2 subset

--- langProBe/AppWorld/AppWorld_program.py
+++ langProBe/AppWorld/AppWorld_program.py
@@ -1,3 +1,4 @@
+import json
 import re
 from typing import List, Tuple
 from .AppWorld_utils.app_world_server_manager import appworld_manager
@@ -6,43 +7,39 @@ import shortuuid
 import dspy
 
 class AppWorldReactSignature(dspy.Signature):
-    """You are a super intelligent AI Assistant whose job is to achieve my day-to-day tasks completely autonomously.
+    """Your job is to achieve my day-to-day tasks completely autonomously.
 
-To do this, you will need to interact with app/s (e.g., spotify, venmo, etc) using their associated APIs on my behalf. For this you will undertake a *multi-step conversation* using a python REPL environment. That is, you will write the python code and the environment will execute it and show you the result, based on which, you will write python code for the next step and so on, until you've achieved the goal. This environment will let you interact with app/s using their associated APIs on my behalf.
+To do this, you will need to interact with app/s (e.g., spotify, venmo etc) using their associated APIs on my behalf. For this you will undertake a *multi-step interaction* using a python REPL environment. That is, you will write the python code and the environment will execute it and show you the result, based on which, you will write python code for the next step and so on, until you've achieved the goal. This environment will let you interact with app/s using their associated APIs on my behalf.
 
 Here are three key APIs that you need to know to get more information:
 
-# 1. To get a list of apps that are available to you.
+# To get a list of apps that are available to you.
 print(apis.api_docs.show_app_descriptions())
 
-# 2. To get the list of apis under any app listed above, e.g. supervisor
-print(apis.api_docs.show_api_descriptions(app_name='supervisor'))
+# To get the list of apis under any app listed above, e.g. spotify
+print(apis.api_docs.show_api_descriptions(app_name='spotify'))
 
-# 3. To get the specification of a particular api, e.g. supervisor app's show_account_passwords
-print(apis.api_docs.show_api_doc(app_name='supervisor', api_name='show_account_passwords'))
+# To get the specification of a particular api, e.g. spotify app's login api
+print(apis.api_docs.show_api_doc(app_name='spotify', api_name='login'))
 
-Your code will be executed in a Python environment that has access to the APIs of the apps, and if anything is printed, it will be shown to you. You can also print variables to see their values. However, if the code raises an exception, you will see the error message.
+Each code execution will produce an output that you can use in subsequent calls. Using these APIs, you can now generate code, that I will execute, to solve the task.
 
-**Key instructions and disclaimers**:
+**Key instructions**:
+(1) Remember you can use the variables in your code in subsequent code blocks.
 
-1. Only generate valid code blocks, i.e., do not put them in ```...``` or add any extra formatting. Any thoughts should be put as code comments.
-2. You can use the variables from the previous code blocks in the subsequent code blocks. To view the variables, you can print them.
-3. Make sure everything is working correctly before making any irreversible change.
-4. The provided Python environment has access to its standard library and the APIs provided by the apps. Do not use any external libraries. To interact with apps, only use the provided APIs, and not the corresponding Python packages. E.g., do NOT use `spotipy` for Spotify.
-5. Any reference to a file system in the task instructions means the file system *app*, operable via given APIs, and not the actual file system the code is running on. So do not write code making calls to os-level modules and functions.
-6. The provided API documentation has both the input arguments and the output JSON schemas. All calls to APIs and parsing its outputs must be as per this documentation.
-7. Different apps may have different login credentials. Read the API documentation for the app to understand how to login.
-8. For APIs that return results in "pages", make sure to consider all pages.
-9. To obtain current data or time, use Python functions like `datetime.now()` or obtain it from the phone app. Do not rely on your existing knowledge of what the current date or time is.
-10. For all temporal requests, use proper time boundaries, e.g., if I ask for something that happened yesterday, make sure to consider the time between 00:00:00 and 23:59:59. All requests are concerning a single, default (no) time zone.
-11. You must make all decisions completely autonomously and not ask for any clarifications or confirmations from me or anyone else.
-12. Any reference to my friends, family or any other person or relation refers to the people in my phone's contacts list.
-13. All my personal information, and information about my app account credentials (like login details), physical addresses and owned payment cards are stored in the "supervisor" app. You can access them via the APIs provided by the supervisor app.
-14. Once you have completed the task, call `apis.supervisor.complete_task()`. If the task asks for some information, return it as the answer argument, i.e. call `apis.supervisor.complete_task(answer=<answer>)`. For tasks that do not require an answer, just skip the answer argument or pass it as None.
-15. The answers, when given, should be just entity or number, not full sentences, e.g., `answer=10` for "How many songs are in the Spotify queue?". When an answer is a number, it should be in numbers, not in words, e.g., "10" and not "ten".
-16. You can also pass `status="fail"` in the complete_task API if you are sure you cannot solve it and want to exit.
-17. Do not mock any data or responses. All data should be fetched from the apps.
-18. Write small chunks of code and only one chunk of code in every step. Make sure everything is working correctly before making any irreversible change."""
+(2) Remember that the email addresses, access tokens and variables (e.g. spotify_password) in the example above are not valid anymore.
+
+(3) You can use the "supervisor" app to get information about my accounts and use the "phone" app to get information about friends and family.
+
+(4) Always look at API specifications (using apis.api_docs.show_api_doc) before calling an API.
+
+(5) Write small chunks of code and only one chunk of code in every step. Make sure everything is working correctly before making any irreversible change.
+
+(6) Many APIs return items in "pages". Make sure to run through all the pages by looping over `page_index`.
+
+(7) Once you have completed the task, make sure to call apis.supervisor.complete_task(). If the task asked for some information, return it as the answer argument, i.e. call apis.supervisor.complete_task(answer=<answer>). Many tasks do not require an answer, so in those cases, just call apis.supervisor.complete_task() i.e. do not pass any argument.
+
+Using these APIs, now generate the next code step to solve the actual task."""
     instruction = dspy.InputField(description="Task to be completed", format=str)
     supervisor_name = dspy.InputField()
     supervisor_phone_number = dspy.InputField()
@@ -51,16 +48,20 @@ Your code will be executed in a Python environment that has access to the APIs o
     code = dspy.OutputField(description="Python code to be executed", format=str)
 
 class AppWorldReact(dspy.Module):
-    def __init__(self, max_steps=40):
-        self.module = dspy.Predict(signature=AppWorldReactSignature)
+    def __init__(self, max_steps=40, module=None, add_few_shot=True):
+        if module is None:
+            self.module = dspy.Predict(signature=AppWorldReactSignature)
+        else:
+            self.module = module
         self.max_steps = max_steps
+        self.add_few_shot = add_few_shot
         self.known_apis = {}
         self.app_docs = {}
     
     def format_trace(self, trace: List[Tuple[str, str]]) -> str:
         # return "\n\n".join([f"Code:\n{code}\nOutput:\n{output}" for code, output in trace])
         if len(trace) == 0:
-            return "# No code executed"
+            return ""
         return "\n\n".join([f"Step {idx+1}\nCode:\n{code}\n\nOutput:\n{output}" for idx, (code, output) in enumerate(trace)])
 
     def forward(self, task_id):
@@ -70,6 +71,21 @@ class AppWorldReact(dspy.Module):
             supervisor_name = task['supervisor']['first_name'] + " " + task['supervisor']['last_name']
             supervisor_email = task['supervisor']['email']
             supervisor_phone = task['supervisor']['phone_number']
+
+            if self.add_few_shot:
+                with open("langProBe/AppWorld/fewshot_trace.json", "r") as f:
+                    past_steps_trace = json.load(f)
+
+                fewshotexample = dspy.Example(
+                    instruction = "How many playlists do I have in Spotify?",
+                    supervisor_name = supervisor_name,
+                    supervisor_phone_number = supervisor_phone,
+                    supervisor_email = supervisor_email,
+                    past_steps = self.format_trace(past_steps_trace[:-1]),
+                    code = past_steps_trace[-1][0],
+                ).with_inputs("instruction", "supervisor_name", "supervisor_phone_number", "supervisor_email", "past_steps")
+
+                self.module.demos = [fewshotexample]
 
             trace = []
             for i in range(self.max_steps):
