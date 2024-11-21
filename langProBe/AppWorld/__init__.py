a568ba210a1b2184370439949df4277223de8f3d
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2024-11-20 20:43:47 -0800

    Improve app world program to get 5/19 (comparable to recoma+gpt4omini) (#30)
    * 5/19 on AppWorld dev _2 subset
    
    * 5/19 and demos added in module init
    
    * 17/57 on AppWorld devset
    
    * add different configuration to appworld
    
    * Add default optimzers to AlfWorld
    
    ---------
    
    Co-authored-by: Shangyint <shangyin@berkeley.edu>

--- langProBe/AppWorld/__init__.py
+++ langProBe/AppWorld/__init__.py
@@ -8,19 +8,24 @@ from langProBe.benchmark import BenchmarkMeta
 
 from langProBe.optimizers import DEFAULT_OPTIMIZERS, OptimizerConfig
 import dspy
+import dspy.teleprompt
 
+appworld_teacher = AppWorldReact(add_few_shot=True)
+appworld_teacher._name = "AppWorldReactAugmented"
+appworld_student = AppWorldReact(add_few_shot=False)
+appworld_student._name = "AppWorldReact"
 
 APPWORLD_OPTIMIZERS = [
     OptimizerConfig(
         optimizer=dspy.teleprompt.BootstrapFewShot,
         init_args=dict(max_errors=1000, max_labeled_demos=0, max_bootstrapped_demos=2),
-        compile_args=dict(),
+        compile_args=dict(teacher=appworld_teacher),
         langProBe_configs=dict(use_valset=False, name="BootstrapFewShot"),
     ),
     OptimizerConfig(
         optimizer=dspy.teleprompt.BootstrapFewShotWithRandomSearch,
         init_args=dict(max_errors=1000, max_labeled_demos=0, max_bootstrapped_demos=2, num_threads=8, num_candidate_programs=8),
-        compile_args=dict(),
+        compile_args=dict(teacher=appworld_teacher),
         langProBe_configs=dict(
             use_valset=True, name="BootstrapFewShotWithRandomSearch"
         ),
@@ -33,12 +38,13 @@ APPWORLD_OPTIMIZERS = [
             num_trials=20,
             max_bootstrapped_demos=2,
             max_labeled_demos=0,
+            teacher=appworld_teacher,
         ),
         langProBe_configs=dict(use_valset=True, name="MIPROv2"),
     ),
 ]
 
 
-benchmark = [BenchmarkMeta(AppWorldBench, [AppWorldReact()], appworld_metric, dataset_mode="full", optimizers=APPWORLD_OPTIMIZERS)]
+benchmark = [BenchmarkMeta(AppWorldBench, [appworld_student, appworld_teacher], appworld_metric, dataset_mode="full", optimizers=APPWORLD_OPTIMIZERS)]
 
 os.environ["APPWORLD_ROOT"] = os.path.join(os.getcwd(), "langProBe", "AppWorld")
