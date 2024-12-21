24d7c67fc48aa71ad4ae2d131f8fa402348d19d6
Cyrus Nouroozi <cyrus@zenbase.ai>
2024-12-21 15:22:03 -0600

    Clean diff

--- langProBe/AlfWorld/AlfWorld_utils/alfworld_server.py
+++ langProBe/AlfWorld/AlfWorld_utils/alfworld_server.py
@@ -9,7 +9,7 @@ from .alfredtwenv_wrapper import AlfredTWEnvOneGame
 app = Flask(__name__)
 
 
-@app.route("/reset", methods=["POST"])
+@app.route('/reset', methods=['POST'])
 def reset():
     try:
         obs, info = game_env.reset()
@@ -19,11 +19,11 @@ def reset():
         return jsonify({"status": "error", "message": traceback.format_exc()})
 
 
-@app.route("/step", methods=["POST"])
+@app.route('/step', methods=['POST'])
 def step():
     try:
         print("Called")
-        action = request.json.get("action")
+        action = request.json.get('action')
         print("Action", action)
         # if not action:
         #     return jsonify({"status": "error", "message": "Action is required"}), 400
@@ -32,17 +32,7 @@ def step():
         print("Result", result)
         assert all(len(x) == 1 for x in result[:-1])
         obs, scores, dones, infos = result
-        return jsonify(
-            {
-                "status": "success",
-                "result": {
-                    "obs": obs[0],
-                    "scores": scores[0],
-                    "dones": dones[0],
-                    "infos": infos,
-                },
-            }
-        )
+        return jsonify({"status": "success", "result": {"obs": obs[0], "scores": scores[0], "dones": dones[0], "infos": infos}})
     except Exception:
         return jsonify({"status": "error", "message": traceback.format_exc()})
 
@@ -56,4 +46,4 @@ if __name__ == "__main__":
 
     game_env = AlfredTWEnvOneGame(gamefile_path=game_filepath).init_env(batch_size=1)
 
-    app.run(host="0.0.0.0", port=portnum, debug=True)
+    app.run(host='0.0.0.0', port=portnum, debug=True)
