f66669cdafa6e36925109ff5c8faf29efb438c03
Cyrus Nouroozi <cyrus@zenbase.ai>
2024-12-21 15:27:25 -0600

    Clean diff

--- langProBe/AppWorld/AppWorld_utils/app_world_client.py
+++ langProBe/AppWorld/AppWorld_utils/app_world_client.py
@@ -96,45 +96,39 @@ class AppWorldClient:
         return self._make_request('GET', '/')
 
     def initialize(self, experiment_name: str, task_id: str):
-        args = AppWorldInitializeArgs(
-            experiment_name=experiment_name, task_id=task_id
-        ).__dict__
+        args = AppWorldInitializeArgs(experiment_name=experiment_name, task_id=task_id).__dict__
         return self._make_request('POST', '/initialize', data=args)
 
     def execute(self, task_id, code):
-        data = {'task_id': task_id, 'code': code}
+        data = {"task_id": task_id, "code": code}
         return self._make_request('POST', '/execute', data=data)
 
     def close(self, task_id):
-        data = {'task_id': task_id}
+        data = {"task_id": task_id}
         return self._make_request('POST', '/close', data=data)
 
     def close_all(self, task_id):
-        data = {'task_id': task_id}
+        data = {"task_id": task_id}
         return self._make_request('POST', '/close_all', data=data)
 
     def evaluate(self, task_id, suppress_errors=False, report=False):
-        data = {
-            'task_id': task_id,
-            'suppress_errors': suppress_errors,
-            'report': report,
-        }
+        data = {"task_id": task_id, "suppress_errors": suppress_errors, "report": report}
         return self._make_request('POST', '/evaluate', data=data)
 
     def save_logs(self, task_id):
-        data = {'task_id': task_id}
+        data = {"task_id": task_id}
         return self._make_request('POST', '/save_logs', data=data)
 
     def save_state(self, task_id, state_id):
-        data = {'task_id': task_id, 'state_id': state_id}
+        data = {"task_id": task_id, "state_id": state_id}
         return self._make_request('POST', '/save_state', data=data)
 
     def load_state(self, task_id, state_id):
-        data = {'task_id': task_id, 'state_id': state_id}
+        data = {"task_id": task_id, "state_id": state_id}
         return self._make_request('POST', '/load_state', data=data)
 
     def task_completed(self, task_id):
-        data = {'task_id': task_id}
+        data = {"task_id": task_id}
         return self._make_request('POST', '/task_completed', data=data)
 
     def show_task(self, task_id):
