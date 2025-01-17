6b292e73e7c09b58bf9ffe9660b322eaf8b9bcef
Shangyint <shangyin@berkeley.edu>
2025-01-17 14:48:56 -0800

    fix minibatch for IreRa

--- langProBe/IReRa/__init__.py
+++ langProBe/IReRa/__init__.py
@@ -8,3 +8,9 @@ subprocess.run(["bash", "langProBe/IReRa/load_data.sh"], capture_output=True, te
 
 programs = [IReRaPredict(), IReRaCOT(), IReRaRetrieve(), IReRaRetrieveRank()]
 benchmark = [BenchmarkMeta(IReRaBench, programs, rp_at_k)]
+
+# making sure minibatch_size is not too large than validation set
+for benchmark_indv in benchmark:
+    for optimizer in benchmark_indv.optimizers:
+        if optimizer.compile_args["minibatch_size"] > 30:
+            optimizer.compile_args["minibatch_size"] = 30
